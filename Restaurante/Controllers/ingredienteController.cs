4f39ca427af38b459d711330b49ee25f45977436
efmontaluisa <58994498+efmontaluisa@users.noreply.github.com>
2019-12-21 22:24:30 -0500

    Demo Maestro Detalle

---
+++ Restaurante/Controllers/ingredienteController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Restaurante;
+
+namespace Restaurante.Controllers
+{
+    public class ingredienteController : Controller
+    {
+        private RestauranteEntities db = new RestauranteEntities();
+
+        // GET: ingrediente
+        public ActionResult Index()
+        {
+            var ingredientes = db.ingredientes.Include(i => i.plato);
+            return View(ingredientes.ToList());
+        }
+
+        // GET: ingrediente/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ingredientes ingredientes = db.ingredientes.Find(id);
+            if (ingredientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredientes);
+        }
+
+        // GET: ingrediente/Create
+        public ActionResult Create()
+        {
+            ViewBag.pla_id = new SelectList(db.plato, "pla_id", "pla_nombre");
+            return View();
+        }
+
+        // POST: ingrediente/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ing_id,pla_id,ing_nombre,ing_cantidad")] ingredientes ingredientes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ingredientes.Add(ingredientes);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.pla_id = new SelectList(db.plato, "pla_id", "pla_nombre", ingredientes.pla_id);
+            return View(ingredientes);
+        }
+
+        // GET: ingrediente/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ingredientes ingredientes = db.ingredientes.Find(id);
+            if (ingredientes == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.pla_id = new SelectList(db.plato, "pla_id", "pla_nombre", ingredientes.pla_id);
+            return View(ingredientes);
+        }
+
+        // POST: ingrediente/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ing_id,pla_id,ing_nombre,ing_cantidad")] ingredientes ingredientes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ingredientes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.pla_id = new SelectList(db.plato, "pla_id", "pla_nombre", ingredientes.pla_id);
+            return View(ingredientes);
+        }
+
+        // GET: ingrediente/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ingredientes ingredientes = db.ingredientes.Find(id);
+            if (ingredientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredientes);
+        }
+
+        // POST: ingrediente/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ingredientes ingredientes = db.ingredientes.Find(id);
+            db.ingredientes.Remove(ingredientes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Restaurante/Controllers/platoController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Restaurante;
+
+namespace Restaurante.Controllers
+{
+    public class platoController : Controller
+    {
+        private RestauranteEntities db = new RestauranteEntities();
+
+        // GET: plato
+        public ActionResult Index()
+        {
+            return View(db.plato.ToList());
+        }
+
+        // GET: plato/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            plato plato = db.plato.Find(id);
+            if (plato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(plato);
+        }
+
+        // GET: plato/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: plato/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "pla_id,pla_nombre,pla_precio_venta")] plato plato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.plato.Add(plato);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(plato);
+        }
+
+        // GET: plato/CreatePlatoCompleto
+        public ActionResult CreatePlatoCompleto()
+        {
+            return View();
+        }
+
+        // POST: plato/CreatePlatoCompleto
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreatePlatoCompleto( plato plato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.plato.Add(plato);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(plato);
+        }
+
+        // GET: plato/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            plato plato = db.plato.Find(id);
+            if (plato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(plato);
+        }
+
+        // POST: plato/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "pla_id,pla_nombre,pla_precio_venta")] plato plato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(plato).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(plato);
+        }
+
+        // GET: plato/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            plato plato = db.plato.Find(id);
+            if (plato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(plato);
+        }
+
+        // POST: plato/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            plato plato = db.plato.Find(id);
+            db.plato.Remove(plato);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
